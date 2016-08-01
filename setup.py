85589b5d06aad7e0ecb0b6b7ff54c4db4697ac30
Hui Peng Hu <woohp135@gmail.com>
2016-08-01 15:04:44 -0700

    add description

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ if 'CC' not in os.environ:
 
 setup(
     name='pdfrender',
+    description='PDF Renderer',
     version='0.1',
     setup_requires=['cffi>=1.0.0'],
     cffi_modules=['pdfrender_build.py:ffi'],
