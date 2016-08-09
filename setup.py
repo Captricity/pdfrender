323c1846a7e73f60df650844cd21cf20d16fdcff
Hui Peng Hu <woohp135@gmail.com>
2016-08-09 13:42:20 -0700

    upgrade version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pdfrender',
     description='PDF Renderer',
-    version='0.1',
+    version='0.2',
     setup_requires=['cffi>=1.0.0'],
     cffi_modules=['pdfrender_build.py:ffi'],
     install_requires=['cffi>=1.0.0'],
