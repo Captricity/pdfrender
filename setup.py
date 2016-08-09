2031846ba99aeb7979f3d332622a22ab397e91ac
Hui Peng Hu <woohp135@gmail.com>
2016-08-09 13:46:43 -0700

    depend on cffi >= 1.7.0

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name='pdfrender',
     description='PDF Renderer',
-    version='0.2',
-    setup_requires=['cffi>=1.0.0'],
+    version='0.2.2',
+    setup_requires=['cffi>=1.7.0'],
     cffi_modules=['pdfrender_build.py:ffi'],
-    install_requires=['cffi>=1.0.0'],
+    install_requires=['cffi>=1.7.0'],
     packages=['pdfrender'],
 )
