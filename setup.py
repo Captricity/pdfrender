dd9844ebc886b0566ad5507bb1405458528d9b88
Marc Abonce Seguin <marca@vidado.ai>
2020-05-18 13:20:20 -0700

    bump to 0.3.1

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class BuildExt(build_ext):
 setup(
     name='pdfrender',
     description='PDF Renderer',
-    version='0.3.0',
+    version='0.3.1',
     setup_requires=['pybind11>=2.2.4', 'Pillow>=4.3.0,<=6.2.2'],
     install_requires=['pybind11>=2.2.4', 'Pillow>=4.3.0,<=6.2.2'],
     ext_modules=ext_modules,
